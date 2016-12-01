21ec4b0aa5dcbe20b9736bf0d7fccbba70313778
Raffaello Bertini <raffaello@kafoodle.com>
2016-12-01 21:26:37 +0000

    ansible wip

---
+++ src/index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Title Goes Here</title></head>
+<body>
+    <?php
+    echo '<h3>PHP for loops to make a silly test</h3>';
+    for($i = 0; $i < 100; $i++) {
+        echo "<p> Run = ${$i}</p>";
+        for($j=0; $j<1000; $j++) {
+            rand();
+            $res = bcsqrt(($j * $i + $j + 1 + $i / $j / $i)+1);
+            rand();
+            $res = bcpow($res, $res);
+        }
+    }
+    ?>
+</body>
+</html>
+
+
