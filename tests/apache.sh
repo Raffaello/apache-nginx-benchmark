db8a158aa28af41e5d34cdab2a7b2a1f7bfd85ff
Raffaello Bertini <raffaello@kafoodle.com>
2016-12-02 18:41:06 +0000

    ansible wip

--- tests/apache.sh
+++ tests/apache.sh
@@ -4,7 +4,7 @@ sudo service nginx stop
 sudo service php71-php-fpm restart
 sudo service httpd start
 
-sudo setenfornce permissive
+#sudo setenfornce permissive
 
 sudo chmod 777 /vagrant/src/index.*
 ab -n 10 -c 4 abtest.dev/index.php
--- tests/nginx.sh
+++ tests/nginx.sh
@@ -4,7 +4,7 @@ sudo service httpd stop
 sudo service php71-php-fpm restart
 sudo service nginx start
 
-sudo setenforce permissive
+#sudo setenforce permissive
 
 sudo chmod 777 /vagrant/src/index.*
 ab -n 10 -c 4 abtest.dev/index.php
