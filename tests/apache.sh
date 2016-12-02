a55c10884c8448ad4913da53a4928c4d23dce860
Raffaello Bertini <raffaello@kafoodle.com>
2016-12-02 21:54:14 +0000

    ansible wip...

--- tests/apache.sh
+++ tests/apache.sh
@@ -8,4 +8,12 @@ sudo service httpd start
 #sudo setenfornce permissive
 
 sudo chmod 777 /vagrant/src/index.*
-ab -n 10 -c 4 abtest.dev/index.php
+
+LOGDIR=/vagrant/tests/logs
+ab -n 1 -c 4 abtest.dev/index.php > ${LOGDIR}/httpd-n1-c4.log
+ab -n 4 -c 2 abtest.dev/index.php > ${LOGDIR}/httpd-n4-c2.log
+ab -n 4 -c 4 abtest.dev/index.php > ${LOGDIR}/httpd-n4-c4.log
+ab -n 10 -c 4 abtest.dev/index.php > ${LOGDIR}/httpd-n10-c4.log
+ab -n 20 -c 8 abtest.dev/index.php > ${LOGDIR}/httpd-n20-c8.log
+ab -n 50 -c 10 abtest.dev/index.php > ${LOGDIR}/httpd-n50-c9.log
+ab -n 100 -c 100 abtest.dev/index.php > ${LOGDIR}/httpd-n10-c4.log
--- tests/nginx.sh
+++ tests/nginx.sh
@@ -8,4 +8,13 @@ sudo service nginx start
 #sudo setenforce permissive
 
 sudo chmod 777 /vagrant/src/index.*
-ab -n 10 -c 4 abtest.dev/index.php
+
+LOGDIR=/vagrant/tests/logs
+ab -n 1 -c 4 abtest.dev/index.php > ${LOGDIR}/nginx-n1-c4.log
+ab -n 4 -c 2 abtest.dev/index.php > ${LOGDIR}/nginx-n4-c2.log
+ab -n 4 -c 4 abtest.dev/index.php > ${LOGDIR}/nginx-n4-c4.log
+ab -n 10 -c 4 abtest.dev/index.php > ${LOGDIR}/nginx-n10-c4.log
+ab -n 20 -c 8 abtest.dev/index.php > ${LOGDIR}/nginx-n20-c8.log
+ab -n 50 -c 10 abtest.dev/index.php > ${LOGDIR}/nginx-n50-c9.log
+ab -n 100 -c 100 abtest.dev/index.php > ${LOGDIR}/nginx-n10-c4.log
+
